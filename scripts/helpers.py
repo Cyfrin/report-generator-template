271658a32cc63306217d0783f98a3b5f3e6e3476
giovannidisiena <giodisiena@gmail.com>
2023-07-17 10:12:30 +0200

    add newlines

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -205,7 +205,7 @@ def get_issues(repository, github):
         summary_tex_content = summary_file.read()
         
     summary_findings_table = ""
-    mitigation_table = f"Name,Status,{get_summary_information()['team_name']},Cyfrin"
+    mitigation_table = f"Name,Status,{get_summary_information()['team_name']},Cyfrin\n"
     for label in SEVERITY_LABELS:
         # Do nothing if there are no issues with this label
         if get_issue_count(issue_dict, label) == 0:
@@ -214,7 +214,7 @@ def get_issues(repository, github):
         fill = math.ceil(math.log10(count_by_severity[label]))
         prefix = f"{label[10:11]}-"
 
-        mitigation_table += f"{label.split()[1]},,,"
+        mitigation_table += f"{label.split()[1]},,,\n"
 
         # Iterate through all findings for the current severity
         for counter, (issue_title, status_label) in enumerate(summary_of_findings[label], start=1):
@@ -222,7 +222,7 @@ def get_issues(repository, github):
             prefixed_title = f"\hyperlink{{{latex_hypertarget}}}{{[{prefix}{str(counter).zfill(fill)}] {format_inline_code(issue_title)}}}"
             status_label = status_label.replace("Report Status: ", "")
             summary_findings_table += f"{prefixed_title} & {status_label} \\\\\n\hline"
-            mitigation_table += f"{issue_title},{status_label},,"
+            mitigation_table += f"{issue_title},{status_label},,\n"
 
     # Replace the placeholder in the SUMMARY_TEX file
     placeholder_start = "% __PLACEHOLDER__SUMMARY_OF_FINDINGS_START"
@@ -368,4 +368,3 @@ def edit_report_md():
         solodit_report.write('\n\n---\n\n# Findings\n')
         for line in source_report:
             solodit_report.write(line)
-            
